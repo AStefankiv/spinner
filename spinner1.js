d98c93c4d4dc620a5213b78b1e64cbae5948cf6c
AStefankiv <stefankif35@gmail.com>
2023-11-11 20:18:12 -0700

    Submit spenner.js

---
+++ spinner1.js
@@ -0,0 +1,61 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 50);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 150);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 250);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 350);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 450);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 550);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r   ');
+}, 650);
+
+setTimeout(() => {
+  process.stdout.write('\r   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r   ');
+}, 750);
